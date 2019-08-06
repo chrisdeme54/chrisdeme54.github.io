a478b7405f634de7a53087ccee73e1f9616d30f6
chrisdeme54 <42049541+chrisdeme54@users.noreply.github.com>
2019-08-06 08:38:25 +0100

    Navbar updated(fixedtop)

---
+++ nav.js
@@ -0,0 +1,11 @@
+alert("hi")
+
+function openNav() {
+  document.getElementById("navbar-toggler").style.width = "250px";
+  document.getElementById("jumbotron").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("navbar-toggler").style.width = "0";
+  document.getElementById("jumbotron").style.marginLeft= "0";
+}
